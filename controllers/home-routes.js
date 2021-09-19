a7067a98876936d17635071f0b483094f6b266fe
Vox Humana <jonprine@gmail.com>
2021-09-19 10:35:16 -0500

    data structure from router

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,22 @@
+
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
-});
+    res.render('homepage',   {
+        "id": 2,
+        "date": "Sept 18",
+        "city": "Atlanta, GA",
+        "venue": "The Eastern",
+        "address": "434 Clifton Dr",
+        "contact": "Scott Orvold",
+        "phone": "223-4433",
+        "email": "scott@zeromile.com",
+        "deal": "$50000",
+        "deposit": "$125,000",
+        "showtime": "8:00pm",
+        "arrivaltime": "5:00pm"
+      });
+  });
 
 module.exports = router;
