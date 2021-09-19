a00792fea5860013d3306113afc7e0af4acc69ba
Vox Humana <jonprine@gmail.com>
2021-09-19 12:41:11 -0500

    added each for mapping

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,8 +5,8 @@ const { Show } = require("../models");
 router.get("/", (req, res) => {
   Show.findAll({})
     .then((dbShowData) => {
-      console.log(dbShowData[0]);
-      res.render('homepage', dbShowData[0].get({ plain: true }));
+      const shows = dbShowData.map(show => show.get({ plain: true }));
+      res.render('homepage', { shows });
     })
     .catch((err) => {
       console.log(err);
